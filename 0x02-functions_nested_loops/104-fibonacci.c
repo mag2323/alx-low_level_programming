45cfbdc3a225a8332a8c4e46e8b8123e3ecc0763
mag2323 <margaretwambui2323@gmail.com>
2023-03-21 11:50:49 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -24,7 +24,7 @@ int main(void)
 
 	for (i = 1; i < 91; i++)
 	{
-		printf(",%lu", aft);
+		printf(", %lu", aft);
 		aft += bef;
 		bef = aft - bef;
 	}
@@ -36,12 +36,12 @@ int main(void)
 
 	for (i = 92; i < 99; ++i)
 	{
-		printf(",%lu", aft1 + (aft2 / l));
+		printf(", %lu", aft1 + (aft2 / l));
 		printf("%lu", aft2 % l);
 		aft1 = aft1 + bef1;
-		bef1 = aft1 + bef1;
+		bef1 = aft1 - bef1;
 		aft2 = aft2 + bef2;
-		bef2 = aft2 + bef2;
+		bef2 = aft2 - bef2;
 	}
 	printf("\n");
 	return (0);
